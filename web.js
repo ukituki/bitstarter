b2404cc3e0a3f4fdb1c7eb64af8cfe4d527f2d6f
ukituki <ukituki@gmail.com>
2013-07-26 20:05:15 +0000

    Added buffer to web.js to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fromHtml = fs.readFileSync('index.html');
+
+buffer1 = new Buffer(256);
+buffer1.write(fromHtml);
+ 
+//fs.readFileSync('index.html'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer1.toString);
 });
 
 var port = process.env.PORT || 5000;
