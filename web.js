4a2a0db4ea368529385aa247537dde7d54c7dc07
ukituki <ukituki@gmail.com>
2013-07-26 20:14:56 +0000

    Added fs.readFileSync() to web.js

--- web.js
+++ web.js
@@ -4,13 +4,11 @@ app.use(express.logger());
 
 var fromHtml = fs.readFileSync('index.html');
 
-buffer1 = new Buffer(256);
-buffer1.write(fromHtml);
- 
-//fs.readFileSync('index.html'));
+//buffer1 = new Buffer(256);
+//buffer1.write(fromHtml);
 
 app.get('/', function(request, response) {
-  response.send(buffer1.toString);
+  response.send('Working?');
 });
 
 var port = process.env.PORT || 5000;
