8f6857cee01673462b9714b8a44a70208378ed7a
ukituki <ukituki@gmail.com>
2013-07-30 20:41:50 +0000

    Edited web.js to port 8080

--- web.js
+++ web.js
@@ -7,15 +7,11 @@ var buff = new Buffer(64);
 var buff = fs.readFileSync('index.html');
 var content = buff.toString();
 
-
-//buffer1 = new Buffer(256);
-//buffer1.write(fromHtml);
-
 app.get('/', function(request, response) {
   response.send(content);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
